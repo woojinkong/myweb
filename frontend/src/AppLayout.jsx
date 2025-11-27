4fa7371d84837013d0ca16a5d2ebf517ed7d13b3
woojinkong <dodejqn6@naver.com>
2025-11-27 10:35:53 +0900

    navbar, shrink수정3

--- frontend/src/AppLayout.jsx
+++ frontend/src/AppLayout.jsx
@@ -38,7 +38,7 @@ export default function AppLayout({ children }) {
         className="main-content"
         style={{
           flex: 1,
-          marginTop: "50px",
+          marginTop: "60px",
           padding: "10px",
           marginLeft: isMobile ? 0 : (isSidebarOpen ? "150px" : "50px"),
           width: isMobile
--- frontend/src/pages/BoardWrite.jsx
+++ frontend/src/pages/BoardWrite.jsx
@@ -284,20 +284,20 @@ const styles = {
   toolbarWrapper: {
   position: "sticky",
   top: "60px",                      // Navbar 높이
-  zIndex: 3000,
+  zIndex: 1950,
   background: "transparent",
   display: "flex",
   justifyContent: "center",
+  maxWidth: "680px",
+  margin: "0 auto",
+  width: "100%",
   },
-
-
-
-
   toolbar: {
   width: "100%",
   maxWidth: "680px",
+  margin: "0 auto",
   background: "#fafafa",
-  padding: "8px",
+  padding: "8px 0",
   borderRadius: "8px",
   border: "1px solid #ddd",
   boxShadow: "0 2px 6px rgba(0,0,0,0.05)",
